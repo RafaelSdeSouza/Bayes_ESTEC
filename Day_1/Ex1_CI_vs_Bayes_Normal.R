2da8f013257684e30e933bf9bc61bcbcf4eb9aaa
Emille Ishida <emilleishida@gmail.com>
2017-12-17 21:21:05 +0100

    ex1

--- Day_1/Ex1_CI_vs_Bayes_Normal.R
+++ Day_1/Ex1_CI_vs_Bayes_Normal.R
@@ -1,21 +1,28 @@
+# ESTEC - 18-20 December 2017
+# by Rafael S. de Souza
+
 # Bayesian vs Frequentist
+# taken from the Python implementation of 
+# http://jakevdp.github.io/blog/2014/06/12/frequentism-and-bayesianism-3-confidence-credibility/
+
 require(mdatools)
 
-N = 5
-Nsamp = 1e6
-sigma_x = 2
+# Example 1: check the outcomes from estimating the 
+#            mean of 5 Gaussian distributed numbers given 
+#            that we have 10^6 samples of each one of them
 
+N = 5                                  # number of random numbers to be averaged
+Nsamp = 1e6                            # number of sample for each random variable
+sigma_x = 2                            # variance of all numbers
 
-x <- matrix(rnorm(Nsamp*5, 0, 2), ncol = 5)
-mu_samp = rowMeans(x)
+x <- matrix(rnorm(Nsamp*5, 0, 2), ncol = 5)          # generate 5 x Nsamp
+mu_samp = rowMeans(x)                                # calculate the mean of each set
 
 
 # Formula
-sig_samp = sigma_x/sqrt(N)
+sig_samp = sigma_x/sqrt(N)                           # Standard error of the mean
 
-cat(sig_samp, "approximates", sd(mu_samp))
-
-
+cat(sig_samp, "approximates", sd(mu_samp))           # 
 
 true_B = 100
 sigma_x = 10
@@ -32,22 +39,14 @@ freq_CI_mu <- function(D,sigma,frac=0.95){
   return(c(mu - Nsigma * sigma_mu, mu + Nsigma * sigma_mu))
 }
   
-
 freq_CI_mu(D,sigma_x)
 
-
-
-
 bayes_CR_mu <- function(D, sigma, frac=0.95){
-Nsigma = sqrt(2)*erfinv(frac)
-mu = mean(x)
-sigma_mu = sigma/sqrt(length(D))
-return(c(mu - Nsigma * sigma_mu, mu + Nsigma * sigma_mu))
+    Nsigma = sqrt(2)*erfinv(frac)
+    mu = mean(x)
+    sigma_mu = sigma/sqrt(length(D))
+    return(c(mu - Nsigma * sigma_mu, mu + Nsigma * sigma_mu))
 }
-
-
-
-
 
 type1 <- rnorm(50, mean = 0, sd = sqrt(2500))
 type2 <- rnorm(50, mean = 0, sd = sqrt(1700))
