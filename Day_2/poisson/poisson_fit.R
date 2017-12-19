26647a91713bb76d2dffbb039f4bdad5687320e8
Emille Ishida <emilleishida@gmail.com>
2017-12-19 11:17:31 +0100

    examples

--- Day_2/poisson/poisson_fit.R
+++ Day_2/poisson/poisson_fit.R
@@ -1,12 +1,13 @@
 # Bayesian tutorial by Rafael S. de Souza - UNC, USA & COIN
 #
 # Partial example from Bayesian Models for Astrophysical Data 
-# by Hilbe, de Souza & Ishida, 2016, Cambridge Univ. Press
+# by Hilbe, de Souza & Ishida, 2017, Cambridge Univ. Press
 #
 # Example of frequentist Poisson regression in R
 # synthetic data
 # 1 response (y) and 1 explanatory variables (x1) with one quadratic term
 
+require(ggplot2)
 
 set.seed(2016)                                        # set seed to replicate example
 nobs <- 75                                          # number of observations
@@ -29,7 +30,6 @@ ggplot(data_pois, aes(x = x1, y = py)) +
   geom_segment(aes(xend = x1, yend = pred_pois)) +
   geom_point(color="purple",size=2) +
   geom_line(aes(x=x1,y=pred_pois))+
-  theme_xkcd() +
   theme(panel.background = element_rect(color = "black", fill = "gray85"),
         panel.grid.major = element_line(colour = "white"),
         panel.grid.minor = element_line(colour = "white")) +
