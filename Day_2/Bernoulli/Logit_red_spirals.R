169db18d6c8ca4bec25b6c4391aba714a632ac5b
Emille Ishida <emilleishida@gmail.com>
2017-12-19 14:15:01 +0100

    red spirals

---
+++ Day_2/Bernoulli/Logit_red_spirals.R
@@ -0,0 +1,126 @@
+# From: Bayesian Models for Astrophysical Data, Cambridge Univ. Press
+# (c) 2017,  Joseph M. Hilbe, Rafael S. de Souza and Emille E. O. Ishida 
+# 
+# you are kindly asked to include the complete citation if you used this 
+# material in a publication
+
+# Code 10.12 - Bernoulli model in R using JAGS, for accessing the relationship between bulge
+#              size and the fraction of red spirals
+
+require(R2jags)
+require(ggplot2)
+source('../../auxiliar_functions/jagsresults.R')
+
+# Data
+path_to_data = '../data/Red_spirals.csv'
+
+# Read data
+Red <- read.csv(path_to_data,header=T)
+
+# Prepare data to JAGS
+N <- nrow(Red)
+x <- Red$fracdeV
+y <- Red$type
+
+# Construct data dictionary
+X <- model.matrix(~ x,
+                   data = Red)
+XX <- model.matrix(~ xx)
+
+K <- ncol(X)
+
+# Prepare data for prediction 
+M=500
+xx = seq(from =  min(x), 
+         to =  max(x), 
+         length.out = M)
+
+
+
+logit_data <- list(Y = y, # response variable
+                   X = X, # predictors
+                   N = N, # sample size
+                   K = K, # number of columns
+                   XX = XX, 
+                   M = M
+)
+
+# Fit
+LOGIT <-"model{
+    # Diffuse normal priors
+    for(i in 1:K){
+        beta[i] ~ dnorm(0, 1e-4)
+    }
+
+    # Likelihood function
+    for (i in 1:N){
+        Y[i] ~ dbern(p[i])
+        logit(p[i]) <- eta[i]
+        eta[i] <- inprod(beta[], X[i,])
+    }
+}"
+
+# Define initial values
+inits <- function () {
+  list(beta = rnorm(ncol(X), 0, 0.1))
+}
+
+# Identify parameters
+params <- c("beta")
+
+# Fit
+LOGIT_fit <- jags(data = logit_data,
+                  inits = inits,
+                  parameters = params,
+                  model = textConnection(LOGIT),
+                  n.thin = 1,
+                  n.chains = 3,
+                  n.burnin = 3000,
+                  n.iter = 6000)
+
+# Output
+print(LOGIT_fit,intervals=c(0.025, 0.975),justify = "left", digits=2)
+
+
+# Plot
+yx <- jagsresults(x=LOGIT_fit, params=c('beta'))
+gdata <- data.frame(x =xx, mean = yx[,"mean"],lwr1=yx[,"25%"],lwr2=yx[,"2.5%"],upr1=yx[,"75%"],upr2=yx[,"97.5%"])
+# Bin data for visualization
+binx<-0.05
+t.breaks <-cut(x, seq(0,0.5, by=binx))
+means <-tapply(y, t.breaks, mean)
+semean <-function(x) sd(x)/sqrt(length(x))
+means.se <-tapply(y, t.breaks, semean)
+
+gbin<-data.frame(x=seq(binx,max(x), by=binx),y=means)
+
+
+
+
+logitmod <-data.frame(y, x) 
+
+
+ggplot(logitmod,aes(x=x,y=y))+ 
+  geom_ribbon(data=gdata,aes(x=xx,ymin=lwr1, ymax=upr1,y=NULL), alpha=0.95, fill=c("#969696"),show.legend=FALSE) +
+  geom_ribbon(data=gdata,aes(x=xx,ymin=lwr2, ymax=upr2,y=NULL), alpha=0.65, fill = c("#d9d9d9"),show.legend=FALSE) +
+  geom_line(data=gdata,aes(x=xx,y=mean),colour="gray25",linetype="dashed",size=1,show.legend=FALSE)+
+  geom_point(aes(x=x,y=y),size=2.5,data=gbin,colour="#de2d26")+
+  geom_errorbar(data=gbin,aes(x=x,y=y,ymin=y-2*means.se,ymax=y+2*means.se),alpha=0.85,
+                colour="#de2d26",width=0.005)+
+  theme_bw() +
+  #  ylab(expression(N[red]/N[Spirals]))+
+  ylab(expression(p[red]))+
+  xlab(expression(FracDev[r]))+
+  theme(legend.background = element_rect(fill="white"),
+        legend.key = element_rect(fill = "white",color = "white"),
+        plot.background = element_rect(fill = "white"),
+        legend.position="top",
+        axis.title.y = element_text(vjust = 0.1,margin=margin(0,10,0,0)),
+        axis.title.x = element_text(vjust = -0.25),
+        text = element_text(size = 25,family="serif"))+
+  coord_cartesian(ylim=c(0,0.4),xlim=c(0,0.5))
+
+quartz.save(type = 'pdf', file = 'LOGIT_red_spirals.pdf',width = 10, height = 9)
+
+
+
