03700813693c701f69d9d4885a1529872e014166
Rafael S. de Souza <rafael.2706@gmail.com>
2017-12-19 14:19:53 +0100

    e

--- Day_2/Bernoulli/Logit_red_spirals.R
+++ Day_2/Bernoulli/Logit_red_spirals.R
@@ -14,20 +14,17 @@ source('../../auxiliar_functions/jagsresults.R')
 # Data
 path_to_data = '../data/Red_spirals.csv'
 
-# Read data
-Red <- read.csv(path_to_data,header=T)
-
 # Prepare data to JAGS
 N <- nrow(Red)
 x <- Red$fracdeV
 y <- Red$type
 
-# Construct data dictionary
-X <- model.matrix(~ x,
-                   data = Red)
-XX <- model.matrix(~ xx)
+gr <- data.frame(frac=x,type=factor(y))
+ggplot(data=gr,aes(x=type,y=frac)) +
+  geom_boxplot() +
+  xlab("red galaxy")
 
-K <- ncol(X)
+
 
 # Prepare data for prediction 
 M=500
@@ -36,59 +33,66 @@ xx = seq(from =  min(x),
          length.out = M)
 
 
+############### Construct data dictionary
+X <- model.matrix(~ x)
+XX <- model.matrix(~ xx)
 
-logit_data <- list(Y = y, # response variable
-                   X = X, # predictors
-                   N = N, # sample size
-                   K = K, # number of columns
+K <- ncol(X)
+logit_data <- list(Y  = y, # Response variable
+                   X  = X,           # Predictors
+                   N  = N,        # Sample size 
                    XX = XX, 
+                   K = K,
                    M = M
 )
+############### JAGS code
+LOGIT<-"model{
 
-# Fit
-LOGIT <-"model{
-    # Diffuse normal priors
-    for(i in 1:K){
-        beta[i] ~ dnorm(0, 1e-4)
-    }
+# Diffuse normal priors for predictors
+for(i in 1:K){
+beta[i]  ~ dnorm(0, 1e-4) 
+}
+# Likelihood function 
 
-    # Likelihood function
-    for (i in 1:N){
-        Y[i] ~ dbern(p[i])
-        logit(p[i]) <- eta[i]
-        eta[i] <- inprod(beta[], X[i,])
-    }
-    # Prediction for new data
-    for (j in 1:M){
-        etax[j]<-inprod(beta[], XX[j,])
-        logit(px[j]) <- etax[j]
-        Yx[j]~dbern(px[j])
-}}"
+for (i in 1:N){  
+Y[i] ~ dbern(p[i])
+logit(p[i]) <- eta[i]
+eta[i]      <- inprod(beta[], X[i,])
 
-# Define initial values
-inits <- function () {
-  list(beta = rnorm(ncol(X), 0, 0.1))
+}
+# Prediction for new data
+for (j in 1:M){
+etax[j]<-inprod(beta[], XX[j,])
+logit(px[j]) <- etax[j]
+Yx[j]~dbern(px[j])
 }
 
-# Identify parameters
-params <- c("beta")
+}
+"
+#A function to generate initial values for mcmc
+inits  <- function () {
+  list(
+    beta  = rnorm(ncol(X), 0, 0.1)  )  }
 
-# Fit
-LOGIT_fit <- jags(data = logit_data,
-                  inits = inits,
-                  parameters = params,
-                  model = textConnection(LOGIT),
-                  n.thin = 1,
-                  n.chains = 3,
-                  n.burnin = 3000,
-                  n.iter = 6000)
+params <- c("beta","px")
 
-# Output
+# Run mcmc
+LOGIT_fit<- jags(data     = logit_data,
+                 inits      = inits,
+                 parameters = params,
+                 model      = textConnection(LOGIT),
+                 n.thin     = 1,
+                 n.chains   = 3,
+                 n.burnin   = 3000,
+                 n.iter     = 6000)
+
+############### Output on screen
 print(LOGIT_fit,intervals=c(0.025, 0.975),justify = "left", digits=2)
 
 
+
 # Plot
-yx <- jagsresults(x=LOGIT_fit, params=c('beta'))
+yx <- jagsresults(x=LOGIT_fit, params=c('px'))
 gdata <- data.frame(x =xx, mean = yx[,"mean"],lwr1=yx[,"25%"],lwr2=yx[,"2.5%"],upr1=yx[,"75%"],upr2=yx[,"97.5%"])
 # Bin data for visualization
 binx<-0.05
@@ -124,8 +128,4 @@ ggplot(logitmod,aes(x=x,y=y))+
         axis.title.x = element_text(vjust = -0.25),
         text = element_text(size = 25,family="serif"))+
   coord_cartesian(ylim=c(0,0.4),xlim=c(0,0.5))
-
-quartz.save(type = 'pdf', file = 'LOGIT_red_spirals.pdf',width = 10, height = 9)
-
-
 
